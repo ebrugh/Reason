46c2d2b8dff95c67c54bb4d27f62fa1ea80a266b
Eleanore Brugh <ebrugh@kent.edu>
2026-02-18 19:24:17 -0500

    Little bit of grouping data

--- Lab1.R
+++ Lab1.R
@@ -1,1 +1,36 @@
 library(tidyverse)
+library(ggplot2)
+library(sf)
+
+p.counties <- "C:/R/Data/CBW/County_Boundaries.shp"
+p.stations <- "C:/R/Data/CBW/Non-Tidal_Water_Quality_Monitoring_Stations_in_the_Chesapeake_Bay.shp"
+
+d.counties <- sf::read_sf(p.counties)
+d.stations <- sf::read_sf(p.stations)
+
+library(dplyr)
+
+glimpse(d.counties)
+
+glimpse(d.stations)
+
+d.stations %>% sf::st_is_valid() 
+
+d.counties %>% dplyr::select(GEOID10, ALAND10) %>% head() 
+
+d.counties %>% dplyr::select(-NAME10) %>% head() 
+
+d.counties %>% dplyr::select(GEOID10:CLASSFP10) %>% head() 
+
+d.counties %>% dplyr::select(-(GEOID10:CLASSFP10)) %>% head()
+
+d.counties %>% dplyr::select(starts_with("C"))
+
+d.counties %>% group_by(STATEFP10) %>% mutate(stateLandArea = sum(ALAND10))
+
+d.counties %>% {
+  as_tibble() %>% dplyr::select(-geometry) %>%
+    group_by(STATEFP10) %>%
+    summarise(stateLandArea = sum(ALAND10))}
+
+
